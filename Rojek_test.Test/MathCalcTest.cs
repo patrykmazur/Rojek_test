927d18ac143e664289a7d723ed53b7cac6a9388e
Patryk Mazur <patryk.mazur95@gmail.com>
2017-04-24 16:57:51 +0200

    AddTest created

---
+++ Rojek_test.Test/MathCalcTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Rojek_test.Test
+{
+    [TestClass]
+    public class MathCalcTest
+    {
+        [TestMethod]
+        public void AddTest()
+        {
+            //arrange
+            MathCalc calc = new MathCalc();
+            var expected = 10;
+
+
+            //act
+            //assert
+        }
+    }
+}
---
+++ Rojek_test/mathCalc.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rojek_test
+{
+    public class MathCalc
+    {
+        public int Add(int a, int b)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
